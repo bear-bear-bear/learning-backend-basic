34a53dedfd7800fecc954ead608b0e35db0e35e5
bearbearbear <bear-bear-bear@kakao.com>
2021-08-16 02:15:16 +0900

    feat: 서버측 코드 작성
    * /, /user, /upload 라우팅
    * morgan, multer, dotenv, cookieParser 적용

---
+++ routes/upload.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const multer = require('multer');
+const router = require('express').Router();
+
+const upload = multer({
+  storage: multer.diskStorage({
+    destination(req, file, done) {
+      done(null, 'uploads/');
+    },
+    filename(req, file, done) {
+      const ext = path.extname(file.originalname);
+      done(null, path.basename(file.originalname, ext) + Date.now() + ext);
+    },
+  }),
+  limits: { fileSize: 5 * 1024 * 1024 },
+});
+
+router.get('/', (req, res) => {
+  const PUBLIC_DIR = req.app.get('PUBLIC_DIR');
+  res.sendFile(path.join(PUBLIC_DIR, 'multipart.html'));
+});
+
+router.post('/', upload.array('image'), (req, res) => {
+  console.log(req);
+  res.send('ok');
+});
+
+module.exports = router;
